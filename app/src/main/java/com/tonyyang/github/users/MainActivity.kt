beb9707a16e8ffa1d8da68930e42eda797a32e27
Tony Yang <superzoro168@gmail.com>
2019-08-01 14:48:42 +0800

    Init project

---
+++ app/src/main/java/com/tonyyang/github/users/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.tonyyang.github.users
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
